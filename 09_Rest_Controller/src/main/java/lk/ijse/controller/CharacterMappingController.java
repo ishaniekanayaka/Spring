834b574df3db6236a21a6f1725c180787824ca0e
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-02-06 10:58:59 +0530

    Character Mapping Controller

--- 09_Rest_Controller/src/main/java/lk/ijse/controller/CharacterMappingController.java
+++ 09_Rest_Controller/src/main/java/lk/ijse/controller/CharacterMappingController.java
@@ -1,4 +1,20 @@
 package lk.ijse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("character")
 public class CharacterMappingController {
+
+    @GetMapping("test1/I???")  //ques marks ganata values danna , meken apita puluwn id wage dewal denna ilakkam gana ema ekka
+    public String test1(){
+        return "test 1 get Invoked method";
+    }
+
+    @GetMapping("????/test2")  //ques marks ganata values danna , meken apita puluwn id wage dewal denna ilakkam gana ema ekka
+    public String test2(){
+        return "test 2 get Invoked method";
+    }
 }
