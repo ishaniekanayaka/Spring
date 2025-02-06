2bb5189b62d37ff9383df225077ebd6da6893342
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-02-06 11:11:04 +0530

    PathVariable  Controller

---
+++ 09_Rest_Controller/src/main/java/lk/ijse/controller/PathVariableController.java
@@ -0,0 +1,31 @@
+package lk.ijse.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("variable")
+
+
+public class PathVariableController {
+    //   http://localhost:8080/09_Rest_Controller_Web_exploded/variable/  meke pitipassatama value add karana eka path variable
+
+   /* @GetMapping(path = "{id}")
+    public String test1(@PathVariable("id") String id){
+        return "Hello ath Variable!!! "+id; //api dena oni value ekak enaawa value nathuw ayawwoth not found
+    }*/
+
+    /*@GetMapping(path = "{id}/{name}")
+    public String test1(@PathVariable("id") String id  , @PathVariable("name") String name){
+        return "Hello ath Variable!!! "+id + name; Hello ath Variable!!! Ishaniakila
+    }*/
+
+
+    @GetMapping(path = "{id:[A][0-9]{3}}")
+    public String test1(@PathVariable("id") String id ){
+        return "Hello ath Variable!!! "+id ;
+    }
+}
