62d2d4a5dcdf6664a688e70b3656783477058a59
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-02-06 10:47:40 +0530

    WildCartMapping Controller

--- 09_Rest_Controller/src/main/java/lk/ijse/controller/WildCardMappingController.java
+++ 09_Rest_Controller/src/main/java/lk/ijse/controller/WildCardMappingController.java
@@ -1,4 +1,22 @@
 package lk.ijse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("wildcard")
 public class WildCardMappingController {
+
+    @GetMapping("test1/*/hello")
+    public String getWildCardMapping1(){
+        return " get WildCard Mapping1 invoked: )";
+    }
+
+    @GetMapping("test2/*/*")
+    public String getWildCardMapping2(){
+        return " get WildCard Mapping2 invoked: )";
+    }
+
 }
+
